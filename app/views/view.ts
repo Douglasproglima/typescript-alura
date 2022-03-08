04014da4a0027de7380f19e8b5bfb597e131caa0
Douglas Lima <Douglasproglima@gmail.com>
2022-03-07 21:06:56 -0300

    Prevent atack script

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,22 @@
 export abstract class View<T> {
     protected _elemento: HTMLElement;
+    private _scape: boolean = false;
 
-    constructor(selectorCss: string) {
+    constructor(selectorCss: string, scape?: boolean) {
         this._elemento = document.querySelector(selectorCss);
+        if(scape) this._scape = scape;
     }
 
     public atualizar(model: T): void {
+        let template = this.template(model);
+        if(this._scape) template = this.RemoverScript(template); 
         this._elemento.innerHTML = this.template(model);
     }
 
     protected abstract template(model: T): string;
+
+    private RemoverScript(scape: string): string {
+        const regex = /<script>[\S\s?]*?<\/script>/;
+        return scape.replace(regex, '');
+    }
 }
